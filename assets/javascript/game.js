98f06322b68e5434fcb5d25f6600c5cc94b2476b
elaintran <elain.ttran@gmail.com>
2019-06-01 15:07:31 -0500

    grouped elements together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -41,16 +41,27 @@ $(document).ready(function() {
     var turn = 1;
     var playerHpElement;
     var enemyHpElement;
+    var enemyRemaining = characterList.length - 1;
 
     //place characters in character select screen
     function gameCharacters() {
         for (var i = 0; i < characterList.length; i++) {
+            var choice = $("<div>");
+            choice.addClass("choice choice-hover");
+            
             var character = $("<div>");
             character.attr("id", characterList[i].id);
-            character.append("<img src='" + characterList[i].image + "'><p>" + characterList[i].name +
-            "</p><span>HP " + characterList[i].hp +"</span>");
-            var choice = $("<div>");
-            choice.addClass("choice");
+            
+            var characterImage = $("<img>");
+            characterImage.attr("src", characterList[i].image);
+            
+            var characterName = $("<p>");
+            characterName.text(characterList[i].name);
+            
+            var characterHP = $("<span>");
+            characterHP.text("HP " + characterList[i].hp);
+            
+            character.append(characterImage).append(characterName).append(characterHP);
             choice.append(character);
             $(".character-select").append(choice);
         }
@@ -62,7 +73,7 @@ $(document).ready(function() {
         if (characterChosen === false && enemyChosen === false) {
             characterChosen = true;
             $(this).addClass("active player-character");
-            //push object into character array
+            //push object into character object
             characterID = $(this).children()[0].id;
             $.each(characterList, function(index) {
                 if (characterList[index].id === characterID) {
@@ -75,9 +86,7 @@ $(document).ready(function() {
         } else if (characterChosen === true && enemyChosen === false) {
             enemyChosen = true;
             $(this).addClass("active enemy-character");
-            //reset object for each enemy
-            enemyObject = [];
-            //push object into enemy array
+            //push object into enemy object
             enemyID = $(this).children()[0].id;
             $.each(characterList, function(index) {
                 if (characterList[index].id === enemyID) {
@@ -89,7 +98,16 @@ $(document).ready(function() {
             if (turn === 1) {
                 var questions = $("<div>");
                 questions.addClass("questions");
-                questions.append("<span class='continue'>Continue</span><span class='restart'>Restart</span>");
+                
+                var continueButton = $("<span>");
+                continueButton.addClass("continue");
+                continueButton.text("Continue");
+                
+                var restartButton = $("<span>");
+                restartButton.addClass("restart");
+                restartButton.text("Restart");
+                
+                questions.append(continueButton).append(restartButton);
                 $(".textbox").prepend(questions);
                 $(".textbox").prepend("<p>* Are you sure you would like to continue with this match up?</p>");
             } else {
@@ -109,13 +127,27 @@ $(document).ready(function() {
     function gameStart() {
         var options = $("<div>");
         options.addClass("options");
-        options.append("<div class='options-items'><span class='attack'>Attack</span><span class='restart'>Restart</span></div>");
+        
+        var optionItems = $("<div>");
+        optionItems.addClass("option-items");
+        
+        var attackButton = $("<span>");
+        attackButton.addClass("attack");
+        attackButton.text("Attack");
+        
+        var restartButton = $("<span>");
+        restartButton.addClass("restart");
+        restartButton.text("Restart");
+        
+        optionItems.append(attackButton).append(restartButton);
+        options.append(optionItems);
         $(".menu").prepend(options);
+        
         $(".textbox").css("width", "77%");
         $(".textbox div").remove();
         $(".textbox p").remove();
-        $(".textbox").prepend("<p>* You have challenged the " + enemyName +
-        " to a battle!</p>");
+        $(".textbox").prepend("<p>* You have challenged the " + enemyName + " to a battle!</p>");
+        
         $(".attack").on("click", function() {
             attack();
         })
@@ -127,8 +159,8 @@ $(document).ready(function() {
     function attack() {
         if (characterObject.hp > 0 && enemyObject.hp > 0) {
             $(".textbox p").remove();
-            $(".textbox").append("<p>* You attacked for " + characterObject.attackPower * turn + " damage.</p>");
-            $(".textbox").append("<p>* " + enemyName + " attacked for " + enemyObject.counterAttackPower + " damage.</p>");
+            $(".textbox").append("<p>* You attacked for <span>" + characterObject.attackPower * turn + "</span> damage.</p>");
+            $(".textbox").append("<p>* " + enemyName + " attacked for <span>" + enemyObject.counterAttackPower + "</span> damage.</p>");
             characterObject.hp = characterObject.hp - enemyObject.counterAttackPower;
             enemyObject.hp = enemyObject.hp - (characterObject.attackPower * turn);
             //selects player hp
@@ -140,6 +172,7 @@ $(document).ready(function() {
             turn++;
         }
         if (characterObject.hp > 0 && enemyObject.hp < 0) {
+            enemyRemaining--;
             win();
             turn++;
         } else if (characterObject.hp <= 0 && enemyObject.hp > 0) {
@@ -147,24 +180,31 @@ $(document).ready(function() {
         }
     }
 
+    //reloads window
+    function restart() {
+        location.reload();
+    }
+
     function win() {
         enemyHpElement.text("HP 0");
-        $(".textbox p").remove();
-        $(".textbox").prepend("<p>* " + enemyName + " have been defeated!</p><p>* Please select the next tetrimino you wish to defeat.</p>");
+        enemyChosen = false;
         $(".enemy-character").remove();
         $(".options").remove();
         $(".textbox").css("width", "100%");
-        enemyChosen = false;  
+        $(".textbox p").remove();
+        //change ending message
+        if (enemyRemaining > 0) {
+            $(".textbox").prepend("<p>* " + enemyName + " have been defeated!</p><p>* Please select the next tetrimino you wish to defeat.</p>");
+        } else {
+            $(".textbox").prepend("<p>* You defeated all of the tetriminos!</p><p>* Would you like to play again?</p>");
+            $("div").removeClass("active");
+            $("div").removeClass("choice-hover");
+        }
     }
 
     function lose() {
         playerHpElement.text("HP 0");
         $(".textbox p").remove();
-        $(".textbox").prepend("<p>* You have been defeated!</p><p>* Press restart to try again!</p>");
-    }
-
-    //reloads window
-    function restart() {
-        location.reload();
+        $(".textbox").prepend("<p>* You have been defeated!</p><p>* Press restart to try again.</p>");
     }
  })
